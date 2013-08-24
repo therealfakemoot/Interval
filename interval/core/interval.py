7e50092637b93de195c10ddfc20031e2c5729b93
Nick Dumas <drakonik@gmail.com>
2013-08-24 11:20:13 -0400

    Add Interval code and tests.
    First commit, adding the semi-complete Interval type and its
    unit tests.

---
+++ interval/core/interval.py
@@ -0,0 +1,31 @@
+from functools import total_ordering
+from planmaker.core.util import ValidationError
+
+@total_ordering
+class Interval(object):
+    '''Interval is a type that allows specifying whether an interval is left
+    or right open/closed. It supports rich comparisons with other Intervals,
+    assuming the values passed as start and end are comparable with each other.
+
+    a < b : Tests whether a precedes b.
+    a <= b : Tests whether a overlaps with b.
+    '''
+     
+    def __init__(self, (start, leftopen), (end, rightopen)):
+        if start > end:
+            raise ValidationError('Start value must preceed end value.')
+        self.start = start
+        self.lopen = leftopen
+        self.end = end
+        self.ropen = rightopen
+
+    def __eq__(self, other):
+        return (self.start,self.end) == (other.start,other.end) and (self.lopen, self.ropen) == (other.lopen, other.ropen)
+
+    def __lt__(self, other):
+        if self.ropen or other.lopen:
+            return self.start <= other.start
+        else:
+            return self.start < other.start
+
+    def __
---
+++ interval/tests/test_interval.py
@@ -0,0 +1,56 @@
+from planmaker.core.interval import Interval
+from planmaker.core.util import ValidationError
+from nose.tools import raises
+from unittest import TestCase
+
+@raises(ValidationError)
+def test_invalid_interval_creation():
+    Interval((1, True),(0, True))
+
+class OpenInterval(TestCase):
+    def setUp(self):
+        self.i = Interval((0, True), (2, True))
+
+    def test_equal_intervals(self):
+        i = Interval((0, True), (2, True))
+        assert self.i == i
+
+    def test_unequal_intervals(self):
+        i = Interval((0, True), (1, True))
+        assert self.i != i
+
+    def test_preceding_interval(self):
+        i = Interval((2, True), (3, True))
+        assert self.i < i
+
+    def test_overlapping_interval(self):
+        i = Interval((2, True), (3, True))
+        assert self.i <= i
+
+    def test_non_overlapping_interval(self):
+        i = Interval((3, True), (4, True))
+        assert not self.i <= i
+
+class LeftClosedInterval(OpenInterval):
+    def setUp(self):
+        self.i = Interval((0, False), (2, True))
+
+    def test_equal_intervals(self):
+        i = Interval((0, False), (2, True))
+        assert self.i == i
+        
+class RightClosedInterval(OpenInterval):
+    def setUp(self):
+        self.i = Interval((0, True), (2, False))
+
+    def test_equal_intervals(self):
+        i = Interval((0, True), (2, False))
+        assert self.i == i
+
+class ClosedInterval(OpenInterval):
+    def setUp(self):
+        self.i = Interval((0, False), (2, False))
+
+    def test_equal_intervals(self):
+        i = Interval((0, False), (2, False))
+        assert self.i == i
